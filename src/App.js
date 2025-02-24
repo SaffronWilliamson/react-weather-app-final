5ed0da2ee66a9391ce9a51aa3054ba2309704f62
SaffronWilliamson <saffron.havana99@gmail.com>
2025-02-24 15:12:40 +0000

    Added date component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Ripponden" />
+        <Weather defaultCity="Manchester" />
         <footer>
           This project was coded by{' '}
           <a
