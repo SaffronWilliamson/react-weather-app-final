c5c493efad75d39bc6deaccf5e773a0aa1684c34
SaffronWilliamson <saffron.havana99@gmail.com>
2025-02-24 11:50:33 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React from 'react';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       Weather app
+      <footer>
+        This project was coded by{' '}
+        <a
+          href="https://www.linkedin.com/in/saffron-williamson/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Saffron Williamson
+        </a>
+        , open sourced on{' '}
+        <a
+          href="https://github.com/SaffronWilliamson/react-weather-app-final"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        , and hosted on{' '}
+        <a
+          href="https://react-weather-app-final.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
-
-
