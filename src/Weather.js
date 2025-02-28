81b13c8a31cf4a59ea653130b51804216a8d3668
SaffronWilliamson <saffron.havana99@gmail.com>
2025-02-28 18:49:11 +0000

    added changing forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
+import './Weather.css';
 import WeatherInfo from './WeatherInfo';
 import WeatherForecast from './WeatherForecast';
 import axios from 'axios';
-import './Weather.css';
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
       ready: true,
-      temperature: response.data.temperature.current,
-      humidity: response.data.temperature.humidity,
+      city: response.data.city,
       date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
       icon: response.data.condition.icon,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
-      city: response.data.city,
     });
   }
 
-  function search() {
+  function searchCity() {
     const apiKey = 'ce7c3ee7eo0a7079t0e5079cd0efcb43';
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
@@ -29,10 +30,10 @@ export default function Weather(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    search();
+    searchCity();
   }
 
-  function handleCityChange(event) {
+  function handleChange(event) {
     setCity(event.target.value);
   }
 
@@ -44,27 +45,28 @@ export default function Weather(props) {
             <div className="col-9">
               <input
                 type="search"
-                placeholder="Enter a city.."
+                placeholder="Enter a city..."
                 className="form-control"
                 autoFocus="on"
-                onChange={handleCityChange}
+                onChange={handleChange}
               />
             </div>
             <div className="col-3">
               <input
                 type="submit"
-                value="search"
+                value="Search"
                 className="btn btn-primary w-100"
               />
             </div>
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast data={weatherData} />
       </div>
     );
   } else {
-    search();
-    return 'The app is loading...';
+    searchCity();
+
+    return 'This app is loading. . .';
   }
 }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
-import WeatherForecastDay from './WeatherForecastDay';
+import React, { useState, useEffect } from 'react';
 import './WeatherForecast.css';
 import axios from 'axios';
+import WeatherForecastDay from './WeatherForecastDay';
 
-export default function WeatherForecats() {
+export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
-  function handleResponse(response) {
-    setForecast(response.data.daily);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.data]);
+
+  function getResponse(response) {
     setLoaded(true);
+    setForecast(response.data.daily);
+  }
+
+  function loadForecast() {
+    let city = props.data.city;
+    const apiKey = 'ce7c3ee7eo0a7079t0e5079cd0efcb43';
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(getResponse);
   }
 
   if (loaded) {
@@ -18,24 +29,22 @@ export default function WeatherForecats() {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 6 && index > 0) {
+            if (index > 0 && index < 6) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = 'ce7c3ee7eo0a7079t0e5079cd0efcb43';
-    let lon = -2.2451148;
-    let lat = 53.4794892;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
+    loadForecast();
 
-    axios.get(apiUrl).then(handleResponse);
     return null;
   }
 }
