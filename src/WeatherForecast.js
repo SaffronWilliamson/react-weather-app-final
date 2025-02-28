08263471268c0c25bfbd15e2e9ed1a02f8359dcd
SaffronWilliamson <saffron.havana99@gmail.com>
2025-02-28 16:01:33 +0000

    added future day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,9 +17,15 @@ export default function WeatherForecats() {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6 && index > 0) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
